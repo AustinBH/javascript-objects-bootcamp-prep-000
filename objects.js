73c7e75ccac723bcbbd3394baf5f281074482430
Austin Harlow <harlowaustin2@gmail.com>
2019-04-27 23:44:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,11 @@
 var playlist = new Object({'The Police': "So Lonely" });
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, artistName, songTitle)
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistname) {
+  delete playlist[artistName]
+  return playlist
 }
